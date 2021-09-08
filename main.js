354ca3379c7b7945157c0d746ba4445861b987f4
Adriy Natalushko <n.andriy@i.ua>
2021-09-08 17:43:37 +0300

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+import galleryItems from './gallery-items.js';
+console.log(galleryItems);
+
+const galleryItemsEl = document.querySelector('.js-gallery');
+const modalImgEl = document.querySelector('lightbox__image');
+const modalEl = document.querySelector('lightbox');
+const buttonEl = document.querySelector('lightbox__button');
