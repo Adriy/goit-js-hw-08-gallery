b3af6ed0e89f2460c9d5d8e8ffd24e1ed918e5c3
Adriy Natalushko <n.andriy@i.ua>
2021-09-13 17:43:55 +0300

    1-ша частина

--- main.js
+++ main.js
@@ -1,7 +1,59 @@
-import galleryItems from './gallery-items.js';
-console.log(galleryItems);
+import gallery from './gallery-items.js';
 
-const galleryItemsEl = document.querySelector('.js-gallery');
-const modalImgEl = document.querySelector('lightbox__image');
-const modalEl = document.querySelector('lightbox');
-const buttonEl = document.querySelector('lightbox__button');
+import refs from './referensec/refs.js';
+
+const { list, modal, lightBoxImage, modalCloseBtn, modalCloseOverlay } = refs;
+
+function createItems(array) {
+  return array
+    .map(elem => {
+      const { preview, original, description } = elem;
+      return `
+    <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+const markup = createItems(gallery);
+
+list.insertAdjacentHTML('afterbegin', markup);
+
+//open
+
+list.addEventListener('click', e => {
+  if (e.target.nodeName === 'IMG') {
+    modal.classList.add('is-open');
+
+    lightBoxImage.src = e.target.dataset.source;
+    lightBoxImage.alt = e.target.alt;
+    e.preventDefault();
+  }
+});
+
+//close
+modal.addEventListener('click', e => {
+  if (e.target.classList.contains('lightbox__overlay')) {
+    modal.classList.remove('is-open');
+  }
+});
+
+window.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+    modal.classList.remove('is-open');
+  }
+});
+
+modalCloseBtn.addEventListener('click', () => {
+  modal.classList.remove('is-open');
+});
---
+++ referensec/refs.js
@@ -0,0 +1,13 @@
+export default {
+  //list
+  list: document.querySelector('.js-gallery'),
+
+  //modal
+  modal: document.querySelector('.js-lightbox'),
+
+  lightBoxImage: document.querySelector('lightbox__image'),
+
+  modalCloseBtn: document.querySelector('[data-action="close-lightbox"]'),
+
+  modalCloseOverlay: document.querySelector('.lightbox__button'),
+};
